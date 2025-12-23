85e9bb0e20d043812820c4f48573aeb14811c9aa
alifa-235 <alifadeldjangouri@gmail.com>
2025-12-23 17:59:13 +0100

    commit LSP

---
+++ ict301/solid/LSP/ViolationDeFactoring/MainLSP.java
@@ -0,0 +1,13 @@
+public class MainLSP {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle();
+        rectangle.setWidth(5);
+        rectangle.setHeight(4);
+        System.out.println("Aire du Rectangle = " + rectangle.getArea());
+        
+        Rectangle rectangle1 = new Square();
+        rectangle1.setWidth(5);
+        rectangle1.setHeight(4);
+        System.out.println("Aire du Carré = " + rectangle1.getArea());
+    }
+}
